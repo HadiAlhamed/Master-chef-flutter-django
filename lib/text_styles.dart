445db038028ff52820ac4e2428c3e7546278c5aa
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-03-31 05:52:12 +0300

    add basic drawer to menu page

---
+++ lib/text_styles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const TextStyle subtitleTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  color: Colors.grey,
+  fontFamily: "Lobster",
+);
+const TextStyle btitleTextStyle2 = TextStyle(
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+  fontFamily: "Lobster",
+);
+const TextStyle atitleTextStyle2 = TextStyle(
+  fontWeight: FontWeight.w500,
+  color: Colors.amber,
+  fontFamily: "Lobster",
+);
