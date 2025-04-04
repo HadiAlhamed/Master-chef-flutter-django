16bf741d9fb4ebb91a1b4a367a4622bd512aac79
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-04-04 03:03:14 +0300

    add menuItemController

---
+++ lib/controllers/menu_item_controller.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+import 'package:testing_api/models/menu_item.dart';
+
+class MenuItemController extends GetxController {
+  List<MenuItem> menuItems = [];
+  List<bool> deleted = List.generate(
+    1050,
+    (item) => false,
+  );
+  bool needUpdate = true;
+  void addMenuItem({required MenuItem menuItem}) {
+    menuItems.add(menuItem);
+  }
+
+  void deleteMenuItem(int index) {
+    deleted[index] = true;
+    update();
+  }
+
+  void changeNeedUpdate(bool value) {
+    needUpdate = value;
+    // update();
+  }
+
+  void clear() {
+    needUpdate = true;
+    menuItems.clear();
+    deleted = List.generate(
+      1050,
+      (item) => false,
+    );
+  }
+}
