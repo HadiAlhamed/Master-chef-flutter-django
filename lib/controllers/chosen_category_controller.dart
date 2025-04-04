3afb593381e8d7affaa94f15ebf39c454ee82dba
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-04-04 03:02:21 +0300

    update categoryController

--- lib/controllers/chosen_category_controller.dart
+++ lib/controllers/chosen_category_controller.dart
@@ -2,7 +2,22 @@ import 'package:get/state_manager.dart';
 
 class ChosenCategoryController extends GetxController {
   RxString chosen = "".obs;
+  List<String> categoriesName = [];
+  Map<String, int> idOfCategory = <String, int>{};
   void changeCategory({required String category}) {
     chosen.value = category;
+  }
+
+  void addCategoryName({required String name}) {
+    categoriesName.add(name);
+  }
+
+  void setCategoryId({required String name, required int id}) {
+    idOfCategory[name] = id;
+  }
+
+  void clear() {
+    categoriesName.clear();
+    idOfCategory.clear();
   }
 }
