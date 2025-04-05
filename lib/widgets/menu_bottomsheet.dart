05554b434a2586f061a86309057a5dba4651f870
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-04-06 01:27:19 +0300

    update menu bottom sheet

--- lib/widgets/menu_bottomsheet.dart
+++ lib/widgets/menu_bottomsheet.dart
@@ -37,138 +37,147 @@ class MenuBottomsheet extends StatelessWidget {
         category: menuItem!.category!.title,
       );
     }
-    return Form(
-      key: globalKey,
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              menuItem == null ? "New Menu Item Info" : "Update Menu Item Info",
-              style: btitleTextStyle1,
-            ),
-            const SizedBox(height: 16),
-            AuthInput(
-              title: 'Title',
-              controller: titleController,
-            ),
-            const SizedBox(height: 16),
-            AuthInput(
-              title: 'Price',
-              controller: priceController,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Obx(() {
-              return RadioListTile<bool>(
-                title: const Text(
-                  "Featured",
-                  style: btitleTextStyle2,
-                ),
-                value: true,
-                groupValue: featuredController.isFeatured.value,
-                onChanged: (bool? value) {
-                  featuredController.changeFeatured();
-                },
-              );
-            }),
-            Obx(
-              () {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      child: Form(
+        key: globalKey,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            // crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                menuItem == null
+                    ? "New Menu Item Info"
+                    : "Update Menu Item Info",
+                style: btitleTextStyle1,
+              ),
+              const SizedBox(height: 16),
+              AuthInput(
+                title: 'Title',
+                controller: titleController,
+              ),
+              const SizedBox(height: 16),
+              AuthInput(
+                title: 'Price',
+                controller: priceController,
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Obx(() {
                 return RadioListTile<bool>(
                   title: const Text(
-                    "Not Featured",
+                    "Featured",
                     style: btitleTextStyle2,
                   ),
-                  value: false,
+                  value: true,
                   groupValue: featuredController.isFeatured.value,
                   onChanged: (bool? value) {
                     featuredController.changeFeatured();
                   },
                 );
-              },
-            ),
-            const SizedBox(height: 16),
-            Obx(() {
-              return DropdownButtonFormField<String>(
-                decoration: InputDecoration(
-                  // 🔹 (2) Styles the input field
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ), // Adds a visible border around the dropdown
-                  contentPadding: const EdgeInsets.symmetric(
-                      horizontal: 12), // Adds padding inside the field
-                ),
-                isExpanded: true,
-                validator: (value) => value == null || value == ""
-                    ? "Please select a category"
-                    : null,
-                hint: const Text("Choose a category"),
-                value: categoryController.chosen.value,
-                items: categoryController.categoriesName
-                    .map((String categoryName) {
-                  return DropdownMenuItem<String>(
-                    value: categoryName,
-                    child: Text(categoryName),
+              }),
+              Obx(
+                () {
+                  return RadioListTile<bool>(
+                    title: const Text(
+                      "Not Featured",
+                      style: btitleTextStyle2,
+                    ),
+                    value: false,
+                    groupValue: featuredController.isFeatured.value,
+                    onChanged: (bool? value) {
+                      featuredController.changeFeatured();
+                    },
                   );
-                }).toList(),
-                onChanged: (newValue) {
-                  categoryController.changeCategory(category: newValue ?? "");
                 },
-              );
-            }),
-            const SizedBox(height: 16),
-            MyButton(
-              onPressed: () async {
-                if (globalKey.currentState!.validate()) {
-                  bool result = menuItem == null
-                      ? await MenuApis.postMenuItem(
-                          menuItem: MenuItem(
-                            categoryId: categoryController
-                                .idOfCategory[categoryController.chosen.value]!,
-                            title: titleController.text.trim(),
-                            price: priceController.text.trim(),
-                            featured: featuredController.isFeatured.value,
-                          ),
-                        )
-                      : await MenuApis.updateMenuItem(
-                          menuItem: MenuItem(
-                            id: menuItem!.id,
-                            categoryId: categoryController
-                                .idOfCategory[categoryController.chosen.value]!,
-                            title: titleController.text.trim(),
-                            price: priceController.text.trim(),
-                            featured: featuredController.isFeatured.value,
-                          ),
-                        );
-                  // Get.back();
+              ),
+              const SizedBox(height: 16),
+              Obx(() {
+                return DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    // 🔹 (2) Styles the input field
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ), // Adds a visible border around the dropdown
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 12), // Adds padding inside the field
+                  ),
+                  isExpanded: true,
+                  validator: (value) => value == null || value == ""
+                      ? "Please select a category"
+                      : null,
+                  hint: const Text("Choose a category"),
+                  value: categoryController.chosen.value,
+                  items: categoryController.categoriesName
+                      .map((String categoryName) {
+                    return DropdownMenuItem<String>(
+                      value: categoryName,
+                      child: Text(categoryName),
+                    );
+                  }).toList(),
+                  onChanged: (newValue) {
+                    categoryController.changeCategory(category: newValue ?? "");
+                  },
+                );
+              }),
+              const SizedBox(height: 16),
+              MyButton(
+                onPressed: () async {
+                  if (globalKey.currentState!.validate()) {
+                    bool result = menuItem == null
+                        ? await MenuApis.postMenuItem(
+                            menuItem: MenuItem(
+                              categoryId: categoryController.idOfCategory[
+                                  categoryController.chosen.value]!,
+                              title: titleController.text.trim(),
+                              price: priceController.text.trim(),
+                              featured: featuredController.isFeatured.value,
+                            ),
+                          )
+                        : await MenuApis.updateMenuItem(
+                            menuItem: MenuItem(
+                              id: menuItem!.id,
+                              categoryId: categoryController.idOfCategory[
+                                  categoryController.chosen.value]!,
+                              title: titleController.text.trim(),
+                              price: priceController.text.trim(),
+                              featured: featuredController.isFeatured.value,
+                            ),
+                          );
+                    // Get.back();
 
-                  if (result) {
-                    print("result $result");
-                    menuController.changeNeedUpdate(true);
-                    print("changed update to true");
-                    categoryController.clear();
-                    print("Navigating ...");
-                    Get.offNamed(
-                      '/menu',
-                      preventDuplicates: false,
-                    );
-                  } else {
-                    Get.snackbar(
-                      'Error',
-                      menuItem == null
-                          ? 'Failed to add new menu item , try later.'
-                          : 'Failed to update menu item , try later.',
-                      snackPosition: SnackPosition.TOP,
-                    );
+                    if (result) {
+                      print("result $result");
+                      menuController.changeNeedUpdate(true);
+                      print("changed update to true");
+                      categoryController.clear();
+                      print("Navigating ...");
+                      Get.offNamed(
+                        '/menu',
+                        preventDuplicates: false,
+                      );
+                    } else {
+                      Get.snackbar(
+                        'Error',
+                        menuItem == null
+                            ? 'Failed to add new menu item , try later.'
+                            : 'Failed to update menu item , try later.',
+                        snackPosition: SnackPosition.TOP,
+                      );
+                    }
                   }
-                }
-              },
-              title: menuItem == null ? 'Add Menu Item' : 'Update Menu Item',
-              color: Colors.amberAccent,
-            )
-          ],
+                },
+                title: menuItem == null ? 'Add Menu Item' : 'Update Menu Item',
+                color: Colors.amberAccent,
+              )
+            ],
+          ),
         ),
       ),
     );
