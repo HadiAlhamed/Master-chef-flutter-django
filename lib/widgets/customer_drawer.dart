1df618b73da9fa6ed45f003465d7a36246a6a79a
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-04-06 01:30:46 +0300

    add initial customer drawer

--- lib/widgets/customer_drawer.dart
+++ lib/widgets/customer_drawer.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:testing_api/views/categories_page.dart';
+import 'package:testing_api/views/customers_page.dart';
+import 'package:testing_api/views/delivery_crew_page.dart';
+import 'package:testing_api/views/menu.dart';
+import 'package:testing_api/widgets/drawer_tile.dart';
 
 class CustomerDrawer extends StatelessWidget {
   const CustomerDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Drawer(
+      child: ListView(
+        padding: const EdgeInsets.all(8),
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(),
+            child: Text(
+              'MasterChef\nCustomer',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: "Lobster",
+                fontWeight: FontWeight.w800,
+                fontSize: 30,
+                color: Colors.amber,
+              ),
+            ),
+          ),
+          DrawerTile(
+              title: "My Orders",
+              subtitle: "view and assign orders to delivery crew",
+              icon: Icons.manage_accounts),
+          DrawerTile(
+            title: "Menu",
+            subtitle: "view all menu items",
+            icon: Icons.menu_book,
+            onTap: () {
+              Get.off(
+                () => Menu(),
+                transition: Transition.leftToRightWithFade,
+                duration: const Duration(milliseconds: 400),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
