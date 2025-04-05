ecc30205979611e46964bcd4af4d3353cd634c44
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-04-06 02:25:52 +0300

    add cart item model

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -6,13 +6,14 @@ class CartItem {
   final String unitPrice;
   final String price;
 
-  CartItem(
-      {required this.cartId,
-      required this.userId,
-      required this.menuItemId,
-      required this.quantity,
-      required this.unitPrice,
-      required this.price});
+  CartItem({
+    required this.cartId,
+    required this.userId,
+    required this.menuItemId,
+    required this.quantity,
+    required this.unitPrice,
+    required this.price,
+  });
 
   factory CartItem.fromJson(Map<String, dynamic> json) {
     return CartItem(
