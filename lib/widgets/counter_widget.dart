036c5c9cf99d76815f2f207077fc8deabe1ba0c9
Hadi Alhamed <hadialhamed.py@gmail.com>
2025-04-06 01:31:39 +0300

    update counter widget : color of icons

--- lib/widgets/counter_widget.dart
+++ lib/widgets/counter_widget.dart
@@ -1,19 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:testing_api/controllers/menu_item_controller.dart';
 import 'package:testing_api/controllers/menu_item_counter_controller.dart';
 
 class CounterWidget extends StatelessWidget {
   final int index;
-  const CounterWidget({
+  CounterWidget({
     super.key,
     required this.index,
   });
 
+  final MenuItemCounterController counterController =
+      Get.find<MenuItemCounterController>();
+  final MenuItemController menuController = Get.find<MenuItemController>();
   @override
   Widget build(BuildContext context) {
-    final MenuItemCounterController counterController =
-        Get.find<MenuItemCounterController>();
     return SizedBox(
       child: Row(
         mainAxisSize: MainAxisSize.min,
@@ -23,7 +25,7 @@ class CounterWidget extends StatelessWidget {
               print("index : $index");
               counterController.decrementCounter(index);
             },
-            icon: const Icon(Icons.remove),
+            icon: const Icon(Icons.remove, color: Colors.red),
           ),
           IconButton(
             onPressed: () {
@@ -32,6 +34,7 @@ class CounterWidget extends StatelessWidget {
             },
             icon: const Icon(
               Icons.add,
+              color: Colors.green,
             ),
           ),
           Obx(() {
